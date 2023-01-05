9d195b85fc585c5079122266cf6ece4947c3effb
selkamand <sam.elkamand@gmail.com>
2023-01-06 03:16:29 +1100

    added undocumented assert function

--- R/assert.R
+++ R/assert.R
@@ -1,11 +1,17 @@
 assert <- function(..., msg = NULL, call = rlang::caller_env()) {
+  all_expressions_list <- eval(substitute(alist(...)))
+  all_expressions_char <- sapply(all_expressions_list, FUN = deparse)
+
   conditions <- list(...)
-  failed_conditions <- conditions[vapply(conditions, function(x) !isTRUE(x), logical(1))]
-  if (length(failed_conditions) > 0) {
+  failed_expressions <- all_expressions_char[vapply(conditions, function(x) !isTRUE(x), logical(1))]
+
+  if (length(failed_expressions) > 0) {
     if (is.null(msg)) {
-      msg <- paste("Error: Assertion failed for the following conditions:", paste(failed_conditions, collapse = ", "))
+      msg <- "Error: The following expression{?s} {?is/are} {.strong NOT TRUE}: [{failed_expressions}]"
     }
     cli::cli_abort(msg, call = call)
   }
   invisible(TRUE)
 }
+
+
