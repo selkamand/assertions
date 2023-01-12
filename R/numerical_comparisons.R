369e74fe396a63b411403d4a84d5d5791806cf63
selkamand <sam.elkamand@gmail.com>
2023-01-12 16:41:26 +1100

    feature: added assert_greater_than and assert_greater_than_or_equal_to

---
+++ R/numerical_comparisons.R
@@ -0,0 +1,48 @@
+#' Assert that the input object is greater than a specified minimum value
+#'
+#' @include assert_create.R
+#' @include is_functions.R
+#' @param x An object to check
+#' @param minimum The minimum value to compare against
+#' @param all_must_satisfy A logical value indicating whether all elements of the vector must be greater than the minimum value or if only one element is sufficient.
+#' @param msg A character string containing the error message to display if `x` is not greater than the specified minimum value
+#' @inheritParams common_roxygen_params
+#'
+#' @return invisible(TRUE) if `x` is greater than the specified minimum value, otherwise aborts with the error message specified by `msg`
+#'
+#' @examples
+#' \dontrun{
+#' assert_greater_than(3, 2) # Passes
+#' assert_greater_than(3, 2, all_must_satisfy = TRUE) # Passes
+#' assert_greater_than(c(2,3,4), 1) # Passes
+#' assert_greater_than(c(2,3,4), 2) # Passes
+#' assert_greater_than(c(2,3,4), 3, all_must_satisfy=FALSE) # Passes
+#' assert_greater_than(c(2,3,1), 3) # Throws default error
+#' assert_greater_than(c(2,3,1), 3, msg = "custom error message") # Throws custom error
+#' }
+#'
+#' @concept assert_comparison
+#' @export
+
+assert_greater_than <- assert_create(
+  func = is_greater_than,
+  default_error_msg = "{.strong {arg_name}} must {ifelse(length(arg_value) > 1, 'all ', '')}be {.strong greater than} `{.strong {minimum}}`."
+)
+
+#' Assert that the input object is greater than or equal to a specified minimum value
+#'
+#' @include assert_create.R
+#' @include is_functions.R
+#' @param x An object to check
+#' @param minimum The minimum value to compare against
+#' @param all_must_satisfy A logical value indicating whether all elements of the vector must be greater than or equal to the minimum value or if only one element is sufficient.
+#' @param msg A character string containing the error message to display if `x` is not greater than or equal to the specified minimum value
+#' @inheritParams common_roxygen_params
+#'
+#' @return invisible(TRUE) if `x` is greater than or equal to the specified minimum value, otherwise aborts with the error message specified by `msg`
+#'
+#' @examples
+#' \dontrun{
+#' assert_greater_than_or_equal_to(3, 2) # Passes
+#' assert_greater_than_or_equal_to(3, 2, all_must_satisfy = TRUE) # Passes
+#' assert_greater_than_or_equal_to
