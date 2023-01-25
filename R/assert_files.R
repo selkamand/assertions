96a049a1f626f1913dc7d1cb694bcbd8debab761
selkamand <sam.elkamand@gmail.com>
2023-01-25 19:32:13 +1100

    added file permission assertions

--- R/assert_files.R
+++ R/assert_files.R
@@ -5,6 +5,20 @@ all_files_exist <- function(x){
   all(file.exists(x))
 }
 
+#' Check file permissions
+#'
+#' Check if the specified filepaths have the specified permissions
+#'
+#' @param filepaths A character vector of file paths to check
+#' @param permission A character string of permission to check for. One of c('write', 'execute', 'read')
+#'
+#' @return A logical value indicating whether all files have the specified permissions
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' has_permission(c("file1.txt", "file2.txt"), permission = "rw")
+#' }
 has_permission <- function(filepaths, permission = c('write', 'execute', 'read')){
   permission_to_mode <- c('write'=2, 'execute'=1, 'read'=4)
   permission <- rlang::arg_match(permission)
@@ -18,6 +32,7 @@ has_permission <- function(filepaths, permission = c('write', 'execute', 'read')
 is_dir <- function(filepaths){
    all(dir.exists(filepaths))
 }
+
 # Files ---------------------------------------------------------------
 #' Assert a file exists
 #'
@@ -68,3 +83,31 @@ assert_directory <- assert_create_chain(
   assert_create(func = all_files_exist, default_error_msg = "Failed to find director{?y/ies}: {.file {arg_value[!file.exists(arg_value)]}}"),
   assert_create(is_dir, default_error_msg = "{.strong {arg_value[!dir.exists(arg_value)]}} {?is a/are} file{?s} not a director{?y/ies}")
   )
+
+#' Assert file permissions
+#'
+#' Assert that all files supplied have the selected read/write/execute permissions
+#'
+#' @include assert_create.R
+#' @include is_functions.R
+#'
+#' @param x An object
+#' @param msg A character string containing the error message if file `x` does not have the specified permissions
+#' @inheritParams common_roxygen_params
+#' @inheritParams has_permission
+#'
+#' @return invisible(TRUE) if `x` exists and has the specified permissions, otherwise aborts with the error message specified by `msg`
+#'
+#' @examples
+#' \dontrun{
+#' assert_file_permissions("file.txt", permissions = "read")
+#' assert_file_permissions("file.txt", permissions = "write") # Throws Error
+#' }
+#'
+#' @concept assert_file
+#'
+#' @export
+assert_file_permissions <- assert_create_chain(
+  assert_file_exists,
+  assert_create(has_permission, default_error_msg = "File {.file {arg_value}} does not have permission {permission}")
+)
