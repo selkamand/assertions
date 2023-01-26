1cecdd5c4ebeb398fb824b7c24d98b1018a69bcd
selkamand <sam.elkamand@gmail.com>
2023-01-26 15:48:32 +1100

    added ability to supply functions that return strings describing error messages

--- tests/testthat/test-assert_create.R
+++ tests/testthat/test-assert_create.R
@@ -33,10 +33,56 @@ cli::test_that_cli(configs = "plain", "assert_create() aborts if default_error_m
   expect_error(assert_create(is.numeric, 1), "1 must be a string (length 1 character vector). Class: numeric; Length: 1", fixed=TRUE)
 })
 
-cli::test_that_cli(configs = "plain", "assertion function aborts if func does not return a logical scalar", {
+cli::test_that_cli(configs = "plain", "assertion function aborts if func does not return a logical scalar when default_error_msg is supplied", {
   assert_is_numeric <- assert_create(function(x) x, "Error: Argument must be numeric")
-  expect_error(assert_is_numeric(2), "Assertion Function `function(x) x` must return TRUE / FALSE. Instead returned: `2`", fixed=TRUE)
+  expect_snapshot(assert_is_numeric(2), error = TRUE)
 })
+
+
+cli::test_that_cli(configs = "plain", "assertion function aborts if func returns FALSE without a default error message", {
+  # Bad assertion: returns FALSE with no default error message
+  bad_assert_no_default_error <- assert_create(function(x) FALSE)
+  expect_snapshot(bad_assert_no_default_error(2), error = TRUE)
+
+  # Good assertion: returns STRING with no default error message
+  good_assert_no_default_error <- assert_create(function(x) "an error message")
+  expect_snapshot(good_assert_no_default_error(2), error = TRUE)
+})
+
+cli::test_that_cli(configs = "plain", "assertion function throws appropriate error when returning neither a flag NOR a string", {
+  # Bad assertion: returns character not string
+  bad_assert_returns_char <- assert_create(function(x) c("a", "b"))
+  expect_snapshot(bad_assert_returns_char("foo"), error = TRUE)
+
+  # Bad assertion: returns logical not flag
+  bad_assert_returns_logical <- assert_create(function(x) c(TRUE, TRUE))
+  expect_snapshot(bad_assert_returns_logical("foo"), error = TRUE)
+
+  # Bad assertion: returns factor
+  bad_assert_returns_factor <- assert_create(function(x) factor(c(1, 4)))
+  expect_snapshot(bad_assert_returns_factor("foo"), error = TRUE)
+
+})
+
+cli::test_that_cli(configs = "plain", "assertion function works as expected with string-returning assertion functions", {
+  # Bad assertion: returns character not string
+  is_between_min_and_max <- function(obj, min, max){
+    if(!is.numeric(obj)) return(paste0("{arg_name} is a {class(arg_value)}, not numeric"))
+
+    if(obj > max) return("{arg_name} is over {max}")
+    else if (obj < min) return("{arg_name} is under {min}")
+
+    return(TRUE)
+  }
+
+  assert_between_min_and_max <- assert_create(is_between_min_and_max)
+  expect_true(assert_between_min_and_max(4, min = 3, max = 5))
+
+  expect_snapshot(assert_between_min_and_max("foo", min = 3, max = 5), error = TRUE)
+  expect_snapshot(assert_between_min_and_max(6, min = 3, max = 5), error = TRUE)
+  expect_snapshot(assert_between_min_and_max(2, min = 3, max = 5), error = TRUE)
+})
+
 
 
 # Test Creation of Assertion Chains  -----------------------------------------------------
