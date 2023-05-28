cac607391079b06b01aa04a275b1120babceb756
Sam El-Kamand <sam.elkamand@gmail.com>
2023-05-28 18:02:23 +1000

    feat: vectorised version of assert_greater than

--- R/is_comparisons.R
+++ R/is_comparisons.R
@@ -51,17 +51,15 @@ compare <- function(x, equal_to = NULL, minimum = NULL, maximum = NULL, comparis
 #'
 #' @param x a numeric vector to check
 #' @param minimum The minimum value to compare against
-#' @param all_must_satisfy A logical value indicating whether all elements of the vector must be greater than the minimum value or if only one element is sufficient. Default is TRUE
 #'
 #' @return A logical value indicating whether all elements of the numeric vector x are greater than the specified minimum value
 #' @examples
 #' is_greater_than(c(2,3,4), 1) # TRUE
 #' is_greater_than(c(2,3,4), 2) # TRUE
-#' is_greater_than(c(2,3,4), 3, all_must_satisfy=FALSE) # TRUE
 #' is_greater_than(c(2,3,1), 3) # FALSE
 #' @export
-is_greater_than <- function(x, minimum, all_must_satisfy = TRUE){
-  compare(x = x, minimum = minimum, all_must_satisfy = all_must_satisfy, comparison_inclusive = FALSE)
+is_greater_than <- function(x, minimum){
+  compare(x = x, minimum = minimum, all_must_satisfy = TRUE, comparison_inclusive = FALSE)
 }
 
 #' Check if a numeric vector is greater than or equal to a specified minimum value
@@ -70,7 +68,6 @@ is_greater_than <- function(x, minimum, all_must_satisfy = TRUE){
 #'
 #' @param x a numeric vector to check
 #' @param minimum The minimum value to compare against
-#' @param all_must_satisfy A logical value indicating whether all elements of the vector must be greater than or equal to the minimum value or if only one element is sufficient. Default is TRUE
 #' @return A logical value indicating whether all elements of the numeric vector x are greater than or equal to the specified minimum value
 #' @examples
 #' is_greater_than_or_equal_to(c(2,3,4), 1) # TRUE
@@ -78,8 +75,8 @@ is_greater_than <- function(x, minimum, all_must_satisfy = TRUE){
 #' is_greater_than_or_equal_to(c(2,3,4), 3, all_must_satisfy=FALSE) # TRUE
 #' is_greater_than_or_equal_to(c(2,3,1), 3) # FALSE
 #' @export
-is_greater_than_or_equal_to <- function(x, minimum, all_must_satisfy = TRUE){
-  compare(x = x, minimum = minimum, all_must_satisfy = all_must_satisfy, comparison_inclusive = TRUE)
+is_greater_than_or_equal_to <- function(x, minimum){
+  compare(x = x, minimum = minimum, all_must_satisfy = TRUE, comparison_inclusive = TRUE)
 }
 
 
--- tests/testthat/test-assert_compare.R
+++ tests/testthat/test-assert_compare.R
@@ -2,9 +2,8 @@ cli::test_that_cli("assert_greater_than() works", config = "plain", {
 
   # Passes
   expect_true(assert_greater_than(3, 2))
-  expect_true(assert_greater_than(3, 2, all_must_satisfy = TRUE))
-  expect_true(assert_greater_than(c(2,3,4), 1))
-  expect_true(assert_greater_than(c(2,3,4), 3, all_must_satisfy=FALSE))
+  expect_true(assert_greater_than(4, 2))
+
 
   # Throws default errors when false
   expect_snapshot(assert_greater_than(c(2,3,1), 3), error = TRUE)
@@ -17,28 +16,60 @@ cli::test_that_cli("assert_greater_than() works", config = "plain", {
   x <- c(2,3,1)
   expect_error(assert_greater_than(x, 3), "x", fixed = TRUE)
 
-  # Passes with numeric input
+  # Passes with number inputs
   expect_true(assert_greater_than(2, 1))
   expect_true(assert_greater_than(2.5, 1))
 
-  # Aborts with non-numeric input
+  # Aborts with non-number input
   expect_snapshot(assert_greater_than('abc', 2), error = TRUE)
   expect_snapshot(assert_greater_than(list(1, 2, 3), 2), error = TRUE)
-  expect_snapshot(assert_greater_than(c(1, 2, 3), 2), error = TRUE)
-  expect_snapshot(assert_greater_than(factor(c(1, 2, 3)), 2), error = TRUE)
+  expect_snapshot(assert_greater_than(c(3, 4, 5), 2), error = TRUE)
+  expect_snapshot(assert_greater_than(factor(4), 2), error = TRUE)
   expect_snapshot(assert_greater_than(TRUE, 2), error = TRUE)
   expect_snapshot(assert_greater_than(NULL, 2), error = TRUE)
 })
 
+cli::test_that_cli("assert_all_greater_than() works", config = "plain", {
+
+  # Passes with number inputs
+  expect_true(assert_all_greater_than(3, 2))
+  expect_true(assert_all_greater_than(4, 2))
+  expect_true(assert_all_greater_than(2, 1))
+  expect_true(assert_all_greater_than(2.5, 1))
+
+  # Passes with numeric inputs
+  expect_true(assert_all_greater_than(c(2, 3, 4), 1))
+
+  # Throws default errors when false
+  expect_snapshot(assert_all_greater_than(c(2,3,1), 3), error = TRUE)
+  expect_snapshot(assert_all_greater_than(c(2,3,4), 2), error = TRUE)
+  expect_snapshot(assert_all_greater_than(2, 2), error = TRUE)
+
+  # Throws custom error
+  expect_error(assert_all_greater_than(c(2,3,1), 3, msg = "custom error message"), "custom error message")
+
+  # Error message uses variable name of passed arguments
+  x <- c(2,3,1)
+  expect_error(assert_all_greater_than(x, 3), "x", fixed = TRUE)
+
+
+  # Aborts with non-number input
+  expect_snapshot(assert_all_greater_than('abc', 2), error = TRUE)
+  expect_snapshot(assert_all_greater_than(list(1, 2, 3), 2), error = TRUE)
+  expect_snapshot(assert_all_greater_than(c(1, 2, 3), 2), error = TRUE)
+  expect_snapshot(assert_all_greater_than(factor(c(1, 2, 3)), 2), error = TRUE)
+  expect_snapshot(assert_all_greater_than(TRUE, 2), error = TRUE)
+  expect_snapshot(assert_all_greater_than(NULL, 2), error = TRUE)
+})
+
+#expect_snapshot(assert_greater_than(c(2,3,4), 1), error = TRUE)
 
 cli::test_that_cli("assert_greater_than_or_equal_to() works", config = "plain", {
 
   # Passes
   expect_true(assert_greater_than_or_equal_to(3, 2))
-  expect_true(assert_greater_than_or_equal_to(c(3, 2, 4), 2, all_must_satisfy = TRUE))
+  expect_true(assert_greater_than_or_equal_to(c(3, 2, 4), 2))
   expect_true(assert_greater_than_or_equal_to(c(2,3,4), 1))
-  expect_true(assert_greater_than_or_equal_to(c(2,3,4), 2, all_must_satisfy=FALSE))
-  expect_true(assert_greater_than_or_equal_to(c(2,3,4), 3, all_must_satisfy=FALSE))
 
   # Throws default errors when false
   expect_snapshot(assert_greater_than_or_equal_to(c(2,3,1), 3), error = TRUE)
