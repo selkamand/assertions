82a26f81b3fb3684b1f251f6737ff1a8d3255a69
Sam El-Kamand <sam.elkamand@gmail.com>
2023-05-28 19:08:17 +1000

    feat: split vector and scalar >= to assertions

--- tests/testthat/test-assert_compare.R
+++ tests/testthat/test-assert_compare.R
@@ -62,17 +62,52 @@ cli::test_that_cli("assert_all_greater_than() works", config = "plain", {
   expect_snapshot(assert_all_greater_than(NULL, 2), error = TRUE)
 })
 
-#expect_snapshot(assert_greater_than(c(2,3,4), 1), error = TRUE)
+
+
+cli::test_that_cli("assert_all_greater_than_or_equal_to() works", config = "plain", {
+
+  # Passes
+  expect_true(assert_all_greater_than_or_equal_to(3, 2))
+  expect_true(assert_all_greater_than_or_equal_to(c(3, 2, 4), 2))
+  expect_true(assert_all_greater_than_or_equal_to(c(2,3,4), 1))
+
+  # Throws default errors when false
+  expect_snapshot(assert_all_greater_than_or_equal_to(c(2,3,1), 3), error = TRUE)
+
+  # Throws custom error
+  expect_error(assert_all_greater_than_or_equal_to(c(2,3,1), 3, msg = "custom error message"), "custom error message")
+
+  # Error message uses variable name of passed arguments
+  x <- c(2,3,1)
+  expect_error(assert_all_greater_than_or_equal_to(x, 3), "x", fixed = TRUE)
+
+  # Passes with numeric input
+  expect_true(assert_all_greater_than_or_equal_to(2, 1))
+  expect_true(assert_all_greater_than_or_equal_to(2, 2))
+  expect_true(assert_all_greater_than_or_equal_to(2.5, 1))
+
+  # Aborts with non-numeric input
+  expect_snapshot(assert_all_greater_than_or_equal_to('abc', 2), error = TRUE)
+  expect_snapshot(assert_all_greater_than_or_equal_to(list(1, 2, 3), 2), error = TRUE)
+  expect_snapshot(assert_all_greater_than_or_equal_to(c(1, 2, 3), 2), error = TRUE)
+  expect_snapshot(assert_all_greater_than_or_equal_to(factor(c(1, 2, 3)), 2), error = TRUE)
+  expect_snapshot(assert_all_greater_than_or_equal_to(TRUE, 2), error = TRUE)
+  expect_snapshot(assert_all_greater_than_or_equal_to(NULL, 2), error = TRUE)
+  expect_snapshot(assert_all_greater_than_or_equal_to(NA, 3), error = TRUE)
+  expect_snapshot(assert_all_greater_than_or_equal_to(c(4, NA), 3), error = TRUE)
+})
 
 cli::test_that_cli("assert_greater_than_or_equal_to() works", config = "plain", {
 
   # Passes
   expect_true(assert_greater_than_or_equal_to(3, 2))
-  expect_true(assert_greater_than_or_equal_to(c(3, 2, 4), 2))
-  expect_true(assert_greater_than_or_equal_to(c(2,3,4), 1))
+  expect_true(assert_greater_than_or_equal_to(2, 2))
+  expect_true(assert_greater_than_or_equal_to(2, 1))
+  expect_true(assert_greater_than_or_equal_to(2.5, 1))
+
 
   # Throws default errors when false
-  expect_snapshot(assert_greater_than_or_equal_to(c(2,3,1), 3), error = TRUE)
+  expect_snapshot(assert_greater_than_or_equal_to(2, 3), error = TRUE)
 
   # Throws custom error
   expect_error(assert_greater_than_or_equal_to(c(2,3,1), 3, msg = "custom error message"), "custom error message")
@@ -81,16 +116,13 @@ cli::test_that_cli("assert_greater_than_or_equal_to() works", config = "plain", 
   x <- c(2,3,1)
   expect_error(assert_greater_than_or_equal_to(x, 3), "x", fixed = TRUE)
 
-  # Passes with numeric input
-  expect_true(assert_greater_than_or_equal_to(2, 1))
-  expect_true(assert_greater_than_or_equal_to(2, 2))
-  expect_true(assert_greater_than_or_equal_to(2.5, 1))
 
-  # Aborts with non-numeric input
+
+  # Aborts with non-number input
   expect_snapshot(assert_greater_than_or_equal_to('abc', 2), error = TRUE)
   expect_snapshot(assert_greater_than_or_equal_to(list(1, 2, 3), 2), error = TRUE)
-  expect_snapshot(assert_greater_than_or_equal_to(c(1, 2, 3), 2), error = TRUE)
-  expect_snapshot(assert_greater_than_or_equal_to(factor(c(1, 2, 3)), 2), error = TRUE)
+  expect_snapshot(assert_greater_than_or_equal_to(c(3, 2, 3), 2), error = TRUE)
+  expect_snapshot(assert_greater_than_or_equal_to(factor(1), 2), error = TRUE)
   expect_snapshot(assert_greater_than_or_equal_to(TRUE, 2), error = TRUE)
   expect_snapshot(assert_greater_than(NULL, 2), error = TRUE)
   expect_snapshot(assert_greater_than_or_equal_to(NA, 3), error = TRUE)
