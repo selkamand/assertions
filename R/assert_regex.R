c64c50e0582a5b91a4b9b7842c0252ba934d549e
selkamand <73202525+selkamand@users.noreply.github.com>
2026-02-08 16:07:41 +1100

    Rename regex assertions and add helpers

---
+++ R/assert_regex.R
@@ -0,0 +1,89 @@
+#' Check if strings contain a regex pattern
+#'
+#' This helper checks whether all elements of a character vector match a regex pattern.
+#'
+#' @param x A character vector to check
+#' @param pattern A regular expression pattern (string)
+#' @param ignore.case,perl,fixed,useBytes Logical flags passed to [grepl()]
+#'
+#' @return TRUE if all strings in `x` match `pattern`, otherwise FALSE or a string error message.
+#'
+#' @concept assert_regex
+contains_pattern <- function(x, pattern, ignore.case = FALSE, perl = FALSE, fixed = FALSE, useBytes = FALSE) {
+  if (!is_string(pattern))
+    return("'pattern' must be a string (length 1 character vector)")
+  if (!is_flag(ignore.case))
+    return("'ignore.case' must be a logical flag")
+  if (!is_flag(perl))
+    return("'perl' must be a logical flag")
+  if (!is_flag(fixed))
+    return("'fixed' must be a logical flag")
+  if (!is_flag(useBytes))
+    return("'useBytes' must be a logical flag")
+
+  all(grepl(pattern, x, ignore.case = ignore.case, perl = perl, fixed = fixed, useBytes = useBytes))
+}
+
+#' Assert all strings contain a regex pattern
+#'
+#' Assert all elements of a character vector match a regex pattern.
+#'
+#' @include assert_create.R
+#' @include assert_type.R
+#' @include has.R
+#' @include is_functions.R
+#' @param x An object to check
+#' @param pattern A regular expression pattern (string)
+#' @param ignore.case,perl,fixed,useBytes Logical flags passed to [grepl()]
+#' @param msg A character string containing the error message to display if `x` does not match `pattern`
+#' @inheritParams common_roxygen_params
+#'
+#' @return invisible(TRUE) if `x` matches `pattern`, otherwise aborts with the error message specified by `msg`
+#'
+#' @examples
+#' try({
+#' assert_all_strings_contain(c("abc", "a1"), "^a") # Passes
+#' assert_all_strings_contain(c("abc", "b1"), "^a") # Throws default error
+#' assert_all_strings_contain(c("abc", "b1"), "^a", msg = "Custom error message") # Throws custom error
+#' })
+#'
+#' @concept assert_regex
+#' @export
+assert_all_strings_contain <- assert_create_chain(
+  assert_character_vector,
+  assert_no_missing,
+  assert_create(
+    func = contains_pattern,
+    default_error_msg = "'{.strong {arg_name}}' must all match regex {.strong {pattern}}"
+  )
+)
+
+#' Assert string contains a regex pattern
+#'
+#' Assert a string matches a regex pattern.
+#'
+#' @include assert_create.R
+#' @include assert_type.R
+#' @include has.R
+#' @include is_functions.R
+#' @param x An object to check
+#' @param pattern A regular expression pattern (string)
+#' @param ignore.case,perl,fixed,useBytes Logical flags passed to [grepl()]
+#' @param msg A character string containing the error message to display if `x` does not match `pattern`
+#' @inheritParams common_roxygen_params
+#'
+#' @return invisible(TRUE) if `x` matches `pattern`, otherwise aborts with the error message specified by `msg`
+#'
+#' @examples
+#' try({
+#' assert_string_contains("abc", "^a") # Passes
+#' assert_string_contains("abc", "^b") # Throws default error
+#' assert_string_contains("abc", "^b", msg = "Custom error message") # Throws custom error
+#' })
+#'
+#' @concept assert_regex
+#' @export
+assert_string_contains <- assert_create_chain(
+  assert_string,
+  assert_all_strings_contain
+)
---
+++ tests/testthat/test-assert_regex.R
@@ -0,0 +1,35 @@
+cli::test_that_cli("assert_all_strings_contain() works", configs = "plain", {
+  # Works for matching strings
+  expect_identical(assert_all_strings_contain(c("abc", "a1"), "^a"), TRUE)
+
+  # Aborts for non-matching strings
+  expect_error(assert_all_strings_contain(c("abc", "b1"), "^a"), "must all match regex", fixed = FALSE)
+
+  # Aborts for non-character inputs
+  expect_error(assert_all_strings_contain(1, "^a"), "character vector", fixed = FALSE)
+
+  # Aborts for invalid pattern inputs
+  expect_error(assert_all_strings_contain("abc", 1), "pattern.*string", fixed = FALSE)
+
+  # Error messages use variable name of passed arguments
+  x <- c("abc", "b1")
+  expect_error(assert_all_strings_contain(x, "^a"), "'x'.*match regex", fixed = FALSE)
+
+  # Custom error messages work
+  expect_error(assert_all_strings_contain(c("abc", "b1"), "^a", msg = "Custom error message"), "Custom error message")
+})
+
+cli::test_that_cli("assert_string_contains() works", configs = "plain", {
+  # Works for matching string
+  expect_identical(assert_string_contains("abc", "^a"), TRUE)
+  expect_identical(assert_string_contains("Abc", "^a", ignore.case = TRUE), TRUE)
+
+  # Aborts for non-matching string
+  expect_error(assert_string_contains("abc", "^b"), "match regex", fixed = FALSE)
+
+  # Aborts for non-string inputs
+  expect_error(assert_string_contains(c("abc", "b1"), "^a"), "string", fixed = FALSE)
+
+  # Custom error messages work
+  expect_error(assert_string_contains("abc", "^b", msg = "Custom error message"), "Custom error message")
+})
